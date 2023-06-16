23315eba9f896591ce9d8e910624d44a4671f77b
Anush79 <barbieworld1056.a@gmail.com>
2023-06-16 19:33:48 +0530

    chore: added Left side bar

---
+++ src/components/LeftSideBar.jsx
@@ -0,0 +1,15 @@
+import {useState} from 'react'
+
+export default function LeftSideBaar (){
+  return <div className="sideBaar">
+    <div className="feed">Feed</div>
+    <div className="search">Search</div>
+    <div className="explore">Explore</div>
+    <div className="bookMarks">Bookmarks</div>
+    <div className="profile">Profile</div>
+    
+    <div className="addTweet">AddTweet</div>
+ 
+ <button>Logout</button>
+  </div>
+}
