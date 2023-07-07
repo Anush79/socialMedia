51b8274267d2f76b36bea008d6188fe611868c3a
Anush79 <barbieworld1056.a@gmail.com>
2023-07-07 21:39:50 +0530

    added color pallete

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -9,22 +9,77 @@ export const testSignUpData = {
   emailId: "kateJohn49@email.com",
   username: "kateJohn",
   password: "johnKate",
-  confirmPassword:"johnKate",
+  confirmPassword: "johnKate",
   bio: "",
   website: "",
-  profileAvatar:"",
-  backgroundImage:"https://res.cloudinary.com/dt1leq0nd/image/upload/v1688371999/tweetopia/hyacinth-1398839_1280_bpodyt.jpg",
+  profileAvatar: "",
+  backgroundImage:
+    "https://res.cloudinary.com/dt1leq0nd/image/upload/v1688371999/tweetopia/hyacinth-1398839_1280_bpodyt.jpg",
 };
 
 export const actionTypes = {
   GET_EVERY_POSTS: "GET_EVERY_POSTS",
-  GET_SINGLE_POST:"GET_SINGLE_POST",
+  GET_SINGLE_POST: "GET_SINGLE_POST",
   GET_ALL_USERS: "GET_ALL_USERS",
-  DELETE_POST:"DELETE_POST",
+  DELETE_POST: "DELETE_POST",
   GET_USER: "GET_USER_WITH_ID",
-  GET_BOOKMARKS:"GET_BOOKMARKS",
-  GET_SUGGESTED_USER:"GET_SUGGESTED_USER",
-  SORT_LATEST_POSTS:"SORT_LATEST_POSTS",
-  SORT_OLDEST_POSTS:"SORT_OLDEST_POSTS",
-  TRENDING_POSTS:"TRENDING_POSTS"
+  GET_BOOKMARKS: "GET_BOOKMARKS",
+  GET_SUGGESTED_USER: "GET_SUGGESTED_USER",
+  SORT_LATEST_POSTS: "SORT_LATEST_POSTS",
+  SORT_OLDEST_POSTS: "SORT_OLDEST_POSTS",
+  TRENDING_POSTS: "TRENDING_POSTS",
+  LIKED_POSTS:"LIKED_POSTS"
 };
+
+export const colorSet = [
+  {
+    "--primary-color": "#fff",
+    "--secondary-color": "#ddd4ff",
+    "--tertiary-color": "#86898d",
+    "--text-color-secondary": "#a598fd",
+    "--text-color": "#650775",
+  },
+  {
+    "--primary-color": "#391E22",
+    "--secondary-color": "#56373C",
+    "--tertiary-color": "#9F807D",
+    "--text-color-secondary": "#D0B0B1",
+    "--text-color": "#E6D1D8",
+  },
+  {
+    "--primary-color": "#2B2A4C",
+    "--secondary-color": "#5c469c8d",
+    "--tertiary-color": "#ad9e9e",
+    "--text-color-secondary": "#D4ADFC",
+    "--text-color": "#E6D1D8",
+  },
+  {
+    "--primary-color": "#C2DEDC",
+    "--secondary-color": "#fff8d9",
+    "--tertiary-color": "#ad9e9e",
+    "--text-color-secondary": "#8BACAA",
+    "--text-color": "#116A7B",
+  },
+  {
+    "--primary-color": "#1A120B",
+    "--secondary-color": "#3C2A21",
+    "--tertiary-color": "#959595",
+    "--text-color-secondary": "#ff9967",
+    "--text-color": "#D5CEA3",
+  },
+  {
+    "--primary-color": "#111",
+    "--secondary-color": "#52525276",
+    "--tertiary-color": "#71767b",
+    "--text-color-secondary": "#1d9bf0",
+    "--text-color": "#fff",
+  },
+  {
+    "--primary-color": "#F8E8EE",
+    "--secondary-color": "#FDCEDF",
+    "--tertiary-color": "#71767b",
+    "--text-color-secondary": "#f287ae",
+    "--text-color": "#650775",
+  },
+
+];
